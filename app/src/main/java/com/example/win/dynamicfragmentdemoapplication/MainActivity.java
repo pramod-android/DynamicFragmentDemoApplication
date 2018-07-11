5c27ae299841a243fcedadc3d3dc3108d7dd88e0
pramod-android <40615535+pramod-android@users.noreply.github.com>
2018-07-11 21:39:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/win/dynamicfragmentdemoapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.win.dynamicfragmentdemoapplication;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addFragment();
+    }
+
+    private void addFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        SampleFragment sampleFragment=new SampleFragment();
+        fragmentTransaction.add(R.id.fragmentContainer,sampleFragment);
+        fragmentTransaction.commit();
+    }
+
+
+}
